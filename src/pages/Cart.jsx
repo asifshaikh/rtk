1c68e2c21337ffe0fd351a5142e967bcb50ca992
asifshaikh <shaikha015@gmail.com>
2025-02-18 22:02:30 +0530

    feat/cartSlice-complete

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,27 @@
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { remove } from '../store/cartSlice';
 const Cart = () => {
-  return <div>Cart</div>;
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart);
+  const handleRemove = (itemId) => {
+    dispatch(remove(itemId));
+  };
+  return (
+    <div>
+      <h3>Cart</h3>
+      {items.map((item) => (
+        <div className='cartCard' key={item.id}>
+          <img src={item.image} alt={item.title} />
+          <h5>{item.title}</h5>
+          <h5>{item.price}</h5>
+          <button className='btn' onClick={() => handleRemove(item.id)}>
+            Remove
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Cart;
