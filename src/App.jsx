a4477c32de6026854ff991839b4417f983c9a5c9
asifshaikh <shaikha015@gmail.com>
2025-02-20 19:21:51 +0530

    persist-data

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Cart from './pages/Cart.jsx';
 import Nav from './components/Nav.jsx';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store';
 function App() {
   return (
     <div>
       <Provider store={store}>
-        <BrowserRouter>
-          <Nav />
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/cart' element={<Cart />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/cart' element={<Cart />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import productReducer from './productSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  product: productReducer,
+  whitelist: ['cart'],
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    product: productReducer,
-  },
+  reducer: persistedReducer,
 });
 export default store;
+
+export const persistor = persistStore(store);
